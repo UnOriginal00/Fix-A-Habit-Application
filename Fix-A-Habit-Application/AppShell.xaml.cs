a19a805d8a122e0f7b6233c74217984dfe1e3392
Pfumi Chauke <pfumichauke4@gmail.com>
2025-12-11 16:18:40 +0200

    Initial commit

---
+++ Fix-A-Habit-Application/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Fix_A_Habit_Application
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Fix-A-Habit-Application/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Fix_A_Habit_Application
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
